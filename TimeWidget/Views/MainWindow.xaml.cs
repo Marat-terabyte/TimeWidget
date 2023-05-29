77a47a84c5620de26a4477bb150d7da3e2602e83
Marat-terabyte <marat.amzph21@gmail.com>
2023-05-29 19:18:11 +0300

    Added methods: 'HideFromAltTab', 'ShoveToBackground'

--- TimeWidget/Views/MainWindow.xaml.cs
+++ TimeWidget/Views/MainWindow.xaml.cs
@@ -1,25 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace TimeWidget
 {
     public partial class MainWindow : Window
     {
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_TOOLWINDOW = 0x00000080;
+
+        public const int HWND_BOTTOM = 0x1;
+        public const uint SWP_NOSIZE = 0x1;
+        public const uint SWP_NOMOVE = 0x2;
+        public const uint SWP_SHOWWINDOW = 0x40;
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(int hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr window, int index, int value);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr window, int index);
+
+        private IntPtr Handle
+        { 
+            get => new WindowInteropHelper(this).Handle;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void WindowActivated(object sender, EventArgs e) => ShoveToBackground();
+
+        private void WindowLoaded(object sender, RoutedEventArgs e) => HideFromAltTab(this.Handle);
+
+
+        private void HideFromAltTab(IntPtr Handle) => SetWindowLong(Handle, GWL_EXSTYLE, GetWindowLong(Handle, GWL_EXSTYLE) | WS_EX_TOOLWINDOW);
+
+        private void ShoveToBackground() => SetWindowPos((int)this.Handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
     }
 }
