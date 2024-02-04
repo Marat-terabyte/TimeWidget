cf2c666aeb391431bfe7049ce570fe7440824c0d
Marat-terabyte <marat.amzph21@gmail.com>
2024-02-04 16:06:59 +0300

    Added

---
+++ TimeWidget/Command/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TimeWidget.Command
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        public RelayCommand(Action<object> action)
+        {
+            _execute = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter!);
+        }
+    }
+}
