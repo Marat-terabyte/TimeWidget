387a89cc086e7bc1bcebf9665a4b2c9813a529dc
Marat-terabyte <marat.amzph21@gmail.com>
2024-02-04 16:19:44 +0300

    Revert "Added parser"
    This reverts commit b52049d27f357ea74031a0790926255987e4db66.

--- TimeWidget/Parsers/IParser.cs
+++ TimeWidget/Parsers/IParser.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TimeWidget.Models;
 
 namespace TimeWidget.Parsers
 {
     internal interface IParser
     {
-        public Weather ParseWeather();
     }
 }
